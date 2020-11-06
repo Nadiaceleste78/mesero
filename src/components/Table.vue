b0680996e1d6442d933a3d6c07160497affdc0f3
feco <hola@fecocode.com>
2020-11-06 10:41:17 -0300

    primera iteración de mesero completa

---
+++ src/components/Table.vue
@@ -0,0 +1,40 @@
+<template>
+  <div 
+    :class="['table', isSelected ? 'selected' : '']"
+    >
+    <h2>{{tableName}}</h2>
+    <h2>{{amountFormatted}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  props: {
+    tableName: String,
+    tableAmount: Number,
+    isSelected: Boolean,
+  },
+  computed: {
+    amountFormatted(){
+      return `$ ${this.tableAmount}`;
+    }
+  }
+}
+</script>
+
+<style>
+  .table{
+    height: 200px;
+    width: 200px;
+    border: 1px solid #ccc;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-content: center;
+    cursor: pointer;
+  }
+  .selected{
+    background: #ddd;
+  }
+</style>
